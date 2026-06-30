075fc2685e19dd44661fa8a9b9da11911263cf82
Victormoex <victorgms2019@gmail.com>
2026-06-30 09:32:35 -0500

    Update Calculadora de resistor.c

--- Calculadora de resistor.c
+++ Calculadora de resistor.c
@@ -21,7 +21,7 @@ int main() {
     
     //validacao para o numeros de resistores
     if (n <= 0 || n > 20) {
-        printf("Quantidade invalida!\n");
+        printf("Quantidade invalida\n");
         return 1;
     }
     //usuario escolhe qual a forma de entrada
@@ -59,19 +59,20 @@ int main() {
         printf("Opcao invalida!\n");
         return 1;
     }
-
+   // pergunta como os resistores estão associados no circuito
     printf("\n1 - Serie\n");
     printf("2 - Paralelo\n");
     printf("3 - Misto\n");
     printf("Escolha: ");
     scanf("%d", &tipo);
-
+    //caso  seja esse serie passara por esse if
     if (tipo == 1) {
         for (i = 0; i < n; i++) {
             Req = Req + R[i];
         }
     }
-
+    //caso seja paralelo passara por esse if
+    
     if (tipo == 2) {
         for (i = 0; i < n; i++) {
             Req = Req + 1.0 / R[i];
@@ -79,19 +80,20 @@ int main() {
 
         Req = 1.0 / Req;
     }
-
+// caso seja associacao mista
     if (tipo == 3) {
+        //pergunta quantos resistores serao em serie
         printf("Quantos resistores em serie? ");
         scanf("%d", &ns);
-
+        //quantos em paralelo
         printf("Quantos resistores em paralelo? ");
         scanf("%d", &np);
-
+        //se os resistores foram maiores do que o usuario escolheu no inicio o programa fecha
         if (ns + np > n) {
-            printf("Quantidade invalida!\n");
+            printf("Quantidade invalida\n");
             return 1;
         }
-
+         // soma os primeiros "ns" resistores do vetor (bloco serie)
         for (i = 0; i < ns; i++) {
             serie = serie + R[i];
         }
@@ -103,18 +105,18 @@ int main() {
         paralelo = 1.0 / paralelo;
         Req = serie + paralelo;
     }
-
+    // checa se tipo foi valido de novo, so detectado tarde,
     if (tipo != 1 && tipo != 2 && tipo != 3) {
-        printf("Opcao invalida!\n");
+        printf("Opcao invalida\n");
         return 1;
     }
 
     printf("\n  RELATORIO FINAL \n");
-
+// lista o valor de cada resistor que foi lido/calculado no inicio
     for (i = 0; i < n; i++) {
         printf("R%d = %.2lf ohms\n", i + 1, R[i]);
     }
-
+// mostra a resistencia equivalente total  calculada conforme o tipo escolhido
     printf("Resistencia equivalente = %.2lf ohms\n", Req);
 
     return 0;
